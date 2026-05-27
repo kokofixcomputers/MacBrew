6c5958bec1b3e89ed2b12ab3f623cc3bc3973456
kokodev <koko@kokodev.cc>
2026-05-26 19:13:11 -0700

    update build step and gitignore to minify

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ rm -rf __pycache__
 rm merged.py
 python -m python_files_merger main.py macbrew_core.py macbrew_constants.py macbrew_livecheck.py macbrew_metadata.py macbrew_network.py macbrew_utils.py macbrew_taps.py macbrew_packages.py macbrew_parsers.py -o merged.py
 python scripts/fix_escape_sequences.py merged.py
-python -c "import py_compile; py_compile.compile('merged.py', 'merged_final.pyc')"
+pyminify merged.py > merged.min.py
+python -c "import py_compile; py_compile.compile('merged.min.py', 'macbrew.pyc')"
